c6a967ea4d36737f140efe2269e7fba8684f5e6a
NothingV4 <zeutronxsite@gmail.com>
2026-09-03 18:43:02 +0700

    Update Scanner.lua

--- Scanner.lua
+++ Scanner.lua
@@ -3,13 +3,16 @@ local isMobile = UserInputService.TouchEnabled and not UserInputService.MouseEna
 local isPC = UserInputService.MouseEnabled and not UserInputService.TouchEnabled
 
 if isMobile then
-info("Protection by Exterminate0, cc8-]>\F6C/V$#uu\wu&$;c)6_0Y<knB4e`76V08,p?QZTNS=true")
+print("Protection by Exterminate0, cc8-]>\F6C/V$#uu\wu&$;c)6_0Y<knB4e`76V08,p?QZTNS=true")
 warn("Protection by Exterminate0, cc8-]>\F6C/V$#uu\wu&$;c)6_0Y<knB4e`76V08,p?QZTNS=true")
 wait(3)
-   loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/CheckingMOBILE/refs/heads/main/Mobile"))()
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/HmgueR8KGDelta/refs/heads/main/Keyboard.Luau"))()
+wait(1)
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/XjXPfzSoLibrary/refs/heads/main/Latest%20Version"))()
+   
 elseif isPC then
-info("Protection by Exterminate0, }0B%\f[3ujxhH+|2Ymd8iICs?OA+'-#O,ltoObyi&AP!H3`-Z=true")
+print("Protection by Exterminate0, }0B%\f[3ujxhH+|2Ymd8iICs?OA+'-#O,ltoObyi&AP!H3`-Z=true")
 warn("Protection by Exterminate0, }0B%\f[3ujxhH+|2Ymd8iICs?OA+'-#O,ltoObyi&AP!H3`-Z=true")
 wait(3)
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/CheckingPC/refs/heads/main/Computer"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/XjXPfzSoLibrary/refs/heads/main/Latest%20Version"))()
 end
