ce822d48c6900201764e1c44bb7ea674225f76fc
NothingV4 <zeutronxsite@gmail.com>
2026-09-03 18:40:35 +0700

    Update Scanner.lua

--- Scanner.lua
+++ Scanner.lua
@@ -1,1 +1,15 @@
+local UserInputService = game:GetService("UserInputService")
+local isMobile = UserInputService.TouchEnabled and not UserInputService.MouseEnabled
+local isPC = UserInputService.MouseEnabled and not UserInputService.TouchEnabled
 
+if isMobile then
+info("Protection by Exterminate0, cc8-]>\F6C/V$#uu\wu&$;c)6_0Y<knB4e`76V08,p?QZTNS=true")
+warn("Protection by Exterminate0, cc8-]>\F6C/V$#uu\wu&$;c)6_0Y<knB4e`76V08,p?QZTNS=true")
+wait(3)
+   loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/CheckingMOBILE/refs/heads/main/Mobile"))()
+elseif isPC then
+info("Protection by Exterminate0, }0B%\f[3ujxhH+|2Ymd8iICs?OA+'-#O,ltoObyi&AP!H3`-Z=true")
+warn("Protection by Exterminate0, }0B%\f[3ujxhH+|2Ymd8iICs?OA+'-#O,ltoObyi&AP!H3`-Z=true")
+wait(3)
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/zeutronxsite/CheckingPC/refs/heads/main/Computer"))()
+end
